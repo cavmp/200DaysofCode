875657e916c62a4ec06dbdfccca91345cbc1e349
cavmp <65962466+cavmp@users.noreply.github.com>
2020-06-01 22:24:09 +0800

    Day 7: Random Name Generator
    This program randomly rearranges names the user inputs. My teachers would always use this to assign the order of presentations in my class. After coding a random number generator, I wanted to step it up a bit so I made this program. I recently also learned about lists and how it works in Python, which was very helpful for this program.

---
+++ Day7-RandomNameGenerator.py
@@ -0,0 +1,22 @@
+import random
+
+def main():
+    """
+    Growing up, my teachers always used this website wherein the computer randomizes the students. After coding
+    a random number generator, I decided to make another program that would randomize names given by the user. 
+    """
+    name_list = [] # Starts off as an empty list as the user hasn't inputted any names
+    while True: # While the user hasn't pressed 'enter'
+        name = input("Add name here. Press the space bar when done adding: ")
+        if name == '': 
+            break
+        name_list.append(name) # The names the user added will be put in the empty list
+
+    print("Here is your list in random order:")
+    for i in range(len(name_list)):
+        random_name_generator = random.choice(name_list) # This randomizes the arrangement of names in the list
+        print(random_name_generator) # Gives the randomized list to user
+
+# This provided line is required at the end of a Python file to call the main() function.
+if __name__ == '__main__':
+    main()
