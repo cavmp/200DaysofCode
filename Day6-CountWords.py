1b2f31583c08c9f3a68b5ba078805070220f43c1
cavmp <65962466+cavmp@users.noreply.github.com>
2020-06-22 04:55:23 +0800

    Day 6: Count Words
    In this program, the user will input characters and will be given back how many words their input has. I decided to do this because counting words for an essay is essential for a student like me. I actually didn't expect this very short code as a result. I learned that the helpful programs don't have to be long and complicated.

--- Day6-CountWords.py
+++ Day6-CountWords.py
@@ -1,12 +1,7 @@
 def main():
     user_input = input("Paste here to count words: ")
-    count = 0
-    for line in user_input:
-        line = line.strip()
-        word_list = line.split()
-        for word in word_list:
-            count +=1 
-    print("Your input contains " + str(count) + " words")
+    word_length = len(user_input.split())
+    print("Your input contains " + str(word_length) + " words")
 
 if __name__ == '__main__':
     main()
