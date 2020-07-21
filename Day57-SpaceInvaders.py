7760f7b15d3d483e5c64cfae4dac9c596e7b875f
Victoria Peliglorio <65962466+cavmp@users.noreply.github.com>
2020-07-21 13:25:26 +0800

    Day 57: Space Invaders
    This is a simple version of the classic 'Space Invaders' game. The user's goal is to get as many invaders (the green object) as s/he can. The left and right buttons are used for changing the direction the player wants to face, and the up button is for increasing the speed.

---
+++ Day57-SpaceInvaders.py
@@ -0,0 +1,104 @@
+import turtle as t
+import math
+import random
+
+# Screen
+window = t.Screen()
+window.title('Space Invaders')
+window.bgpic('images/bg.gif')
+window.bgcolor('black')
+window.tracer(3)
+
+# Border
+mypen = t.Turtle()
+mypen.penup()
+mypen.setposition(-300, -300)
+mypen.color('#5e5656')
+mypen.pendown()
+mypen.pensize(3)
+for side in range(4):
+    mypen.forward(600)
+    mypen.left(90)
+mypen.hideturtle()
+
+# Player
+player = t.Turtle()
+player.color('red')
+player.shape('triangle')
+player.penup()
+
+# Score
+score = 0
+
+# Player's goals
+max_goals = 6
+goals = []
+
+window.register_shape("images/invader.gif")
+
+for count in range(max_goals):
+    goals.append(t.Turtle())
+    goals[count].color('red')
+    goals[count].shape('images/invader.gif')
+    goals[count].penup()
+    goals[count].speed(0)
+    goals[count].setposition(random.randint(-300, 300), random.randint(-300, 300))
+
+speed = 1
+
+def turnleft():
+    player.left(30)
+
+def turnright():
+    player.right(30)
+
+def increasespeed():
+    global speed
+    speed += 1
+
+def isCollision(t1, t2):
+    d = math.sqrt(math.pow(t1.xcor()-t2.xcor(), 2) + math.pow(t1.ycor()-t2.ycor(), 2))
+    if d < 20:
+        return True
+    else:
+        return False
+
+t.listen()
+t.onkey(turnleft, 'Left')
+t.onkey(turnright, 'Right')
+t.onkey(increasespeed, 'Up')
+
+
+while True:
+    player.forward(speed)
+
+    # Boundary
+    if player.xcor() > 300 or player.xcor() < -300:
+        player.right(180)
+
+    if player.ycor() > 300 or player.ycor() <-300:
+        player.right(180)
+
+    # Move the goal
+    for count in range(max_goals):
+        goals[count].forward(3)
+
+        # Boundary check for goal
+        if goals[count].xcor() > 290 or goals[count].xcor() < -290:
+            goals[count].right(180)
+
+        if goals[count].ycor() > 290 or goals[count].ycor() < -290:
+            goals[count].right(180)
+
+        # Check for collisions
+        if isCollision(player, goals[count]):
+            goals[count].setposition(random.randint(-300, 300), random.randint(-300, 300))
+            goals[count].right(random.randint(0, 360))
+            score += 1
+            # Draws the score on screen
+            mypen.undo()
+            mypen.penup()
+            mypen.hideturtle()
+            mypen.setposition(-290, 310)
+            scorestring = "Score: %s" %score
+            mypen.write(scorestring, False, align="left", font=("Arial", 14, "normal"))
